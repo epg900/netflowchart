be727c30b5388ae62b74539db51f756eac8f9dce
Ersci <47410365+epg900@users.noreply.github.com>
2025-09-14 12:59:58 +0330

    Update settings.py

--- netfl/settings.py
+++ netfl/settings.py
@@ -11,7 +11,7 @@ https://docs.djangoproject.com/en/4.2/ref/settings/
 """
 
 from pathlib import Path
-import os
+import os, getpass
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -113,9 +113,9 @@ USE_I18N = True
 
 USE_TZ = True
 
-USER = ''
-PASSWD = ''
-HOST = ''
+USER = input('Enter Username: ')
+PASSWD = getpass.getpass(prompt='Enter Password: ')
+
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
